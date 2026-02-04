925eea943be7f7933db8ff200a19ea4369047d88
Kshiku12 <kshitij.verma302312@gmail.com>
2026-02-04 14:29:23 +0530

    Delete backend directory

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    PGHOST = os.getenv("PGHOST", "ep-yellow-hall-a5tqvaav-pooler.us-east-2.aws.neon.tech")
-    PGDATABASE = os.getenv("PGDATABASE", "neondb")
-    PGUSER = os.getenv("PGUSER", "neondb_owner")
-    PGPASSWORD = os.getenv("PGPASSWORD", "npg_8RY2uXlLzwPI")
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")  # Change this in production
